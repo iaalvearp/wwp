8c3e22ec709e2ed282c4852e93941e06b69568dc
iaalvearp <iaalvearp@gmail.com>
2025-07-13 15:01:53 -0500

    Take out image fill logic to a JS file

---
+++ public/gallery.js
@@ -0,0 +1,14 @@
+import img1 from '/public/imgs/1.avif';
+import img2 from '/public/imgs/2.avif';
+import img3 from '/public/imgs/3.avif';
+import img1Crop from '/public/imgs/1-crop.avif';
+import img2Crop from '/public/imgs/2-crop.avif';
+import img3Crop from '/public/imgs/3-crop.avif';
+
+const gallery = [
+    { picture: img1, src: img1.src, crop: img1Crop.src, alt: 'Image 1' },
+    { picture: img2, src: img2.src, crop: img2Crop.src, alt: 'Image 2' },
+    { picture: img3, src: img3.src, crop: img3Crop.src, alt: 'Image 3' },
+];
+
+export default gallery;
