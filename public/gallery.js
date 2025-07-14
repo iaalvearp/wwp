7e33d9e9a79c58b3b964b770819e27f450401caa
iaalvearp <iaalvearp@gmail.com>
2025-07-13 19:47:30 -0500

    Changing cropped images for thumbnails

--- public/gallery.js
+++ public/gallery.js
@@ -1,9 +1,9 @@
 import img1 from '/public/imgs/1.avif';
 import img2 from '/public/imgs/2.avif';
 import img3 from '/public/imgs/3.avif';
-import img1Crop from '/public/imgs/1-crop.avif';
-import img2Crop from '/public/imgs/2-crop.avif';
-import img3Crop from '/public/imgs/3-crop.avif';
+import img1Crop from '/public/imgs/1-thumb.avif';
+import img2Crop from '/public/imgs/2-thumb.avif';
+import img3Crop from '/public/imgs/3-thumb.avif';
 
 const gallery = [
     { picture: img1, src: img1.src, crop: img1Crop.src, alt: 'Image 1' },
